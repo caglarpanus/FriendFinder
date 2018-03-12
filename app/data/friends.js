0898b53a3c4ac091d098ab1dcd944f0f0c9d7683
caglarpanus <panuscaglar@gmail.com>
2018-03-12 16:50:46 -0400

    home page is done

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friendsArray = [
+    {
+        name:"Lion King",
+        photo:"https://upload.wikimedia.org/wikipedia/commons/1/17/HansomeLion_002.jpg",
+        scores:[5,4,5,3,4,5,4,3,5,1]
+    },
+    {
+        name:"Mr.Penguin",
+        photo:"http://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/istock-511366776.jpg?itok=cWhdWNZ8&resize=1100x619",
+        scores:[4,5,5,1,3,2,4,3,5,5]
+    },
+    {
+        name:"Mrs.Horse",
+        photo:"https://secure.aspca.org/files/aspca/social_images/advo-sideimage-us-horse-slaughter-safe-act-2015_062016_fb.jpg",
+        scores:[4,5,3,5,5,1,5,3,3,1]
+    },
+    {
+        name:"Mr.Owl",
+        photo:"https://youtopiaproject.com/wp-content/uploads/2015/08/Owl.jpg",
+        scores:[3,4,5,3,5,3,4,5,1,1]
+    }
+];
--- server.js
+++ server.js
@@ -6,3 +6,13 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function(){
+    console.log("App is listening on PORT: " + PORT);
+});
