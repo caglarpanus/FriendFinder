dd02d03485b039d9c3246d22097e8ac5d5c3e77e
caglarpanus <panuscaglar@gmail.com>
2018-03-13 10:35:12 -0400

    apiroutes are done

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,4 @@
+
 var friends = require("../data/friends.js");
 
 var bodyParser = require("body-parser");
@@ -5,7 +6,33 @@ var path = require("path");
 
 
 module.exports = function(app){
-    app.get("api/friends", function(req,res){
+    app.get("/api/friends", function(req,res){
         res.json(friends);
+        console.log(friends);
     });
-}
+
+    app.post("/api/friends", function(req,res){
+
+        var newUser = {
+            name:req.body.name,
+            photo:req.body.photo,
+            scores:JSON.parse(req.body.scores)
+        }
+
+        var diffArray =[];
+
+        friends.forEach(function(item,index){
+            var difference = 0;
+            for(var i=0; item.scores.length; i++){
+                difference += Math.abs(item.scores[i] - newUser.scores[i]);
+                diffArray.push({"difference": difference, "index":index});
+            }
+        });
+        diffArray.sort(function(a,b){
+            return a.difference - b.difference;
+        });
+
+        friends.push(newUser);
+        res.json(friends[diffArray[0].index])
+    });
+};
