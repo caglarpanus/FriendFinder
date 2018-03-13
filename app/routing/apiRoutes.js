94b9f94c87273628fc34a0a4efc7a28e170e99c3
caglarpanus <panuscaglar@gmail.com>
2018-03-13 15:46:56 -0400

    small fixes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,36 +1,42 @@
-
+// Dependencies
+// ===============================================================================
+const path = require("path");
 var friends = require("../data/friends.js");
-var path = require("path");
 
+// Routes
+// ===============================================================================
 
-module.exports = function(app){
-    app.get("/api/friends", function(req,res){
+module.exports = function(app) {
+
+    app.get("/api/friends", function(req, res) {
         res.json(friends);
-        console.log(friends);
     });
 
-    app.post("/api/friends", function(req,res){
+    app.post("/api/friends", function(req, res) {
 
-        var newUser = {
+        var newPerson = {
             name: req.body.name,
             photo: req.body.photo,
             scores: JSON.parse(req.body.scores)
         }
 
-        var diffArray =[];
+        var diffArray = [];
 
-        friends.forEach(function(item,index){
+        friends.forEach(function(item, index) {
             var difference = 0;
-            for(var i=0; item.scores.length; i++){
-                difference += Math.abs(item.scores[i] - newUser.scores[i]);
+            for (var i = 0; i < item.scores.length; i++) {
+                difference += Math.abs(item.scores[i] - newPerson.scores[i]);
             }
-            diffArray.push({"difference": difference, "index":index});
+            diffArray.push({ "difference": difference, "index": index });
         });
-        diffArray.sort(function(a,b){
+
+        diffArray.sort(function(a, b) {
             return a.difference - b.difference;
         });
 
-        friends.push(newUser);
-        res.json(friends[diffArray[0].index])
+        friends.push(newPerson);
+        res.json(friends[diffArray[0].index]);
+
     });
-};
+
+}
--- server.js
+++ server.js
@@ -1,19 +1,23 @@
+// Dependencies
+// ==============================================================================
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({extended:true}));
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('app'));
 
-
+// Route requiring
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
-app.listen(PORT, function(){
-    console.log("App is listening on PORT: " + PORT);
+// Listener
+// =============================================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });
